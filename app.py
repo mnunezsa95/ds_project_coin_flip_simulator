df5b54faa805edea03facf167d2c6ef1a99bcb25
mnunezsa <mnunezsa95@gmail.com>
2024-04-04 16:24:13 -0400

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import scipy.stats
 import streamlit as st
 import time
 
-# these are stateful variables which are preserved as Streamlin reruns this script
+# these are stateful variables; preserved as Streamlin reruns this script
 if "experiment_no" not in st.session_state:
     st.session_state["experiment_no"] = 0
 
@@ -12,34 +12,37 @@ if "df_experiment_results" not in st.session_state:
         columns=["no", "iterations", "mean"]
     )
 
+# Header of the application
 st.header("Tossing a Coin")
 
+# Chart widget
 chart = st.line_chart([0.5])
 
 
+# function to toss a coin
 def toss_coin(n):
-
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-
-    mean = None
-    outcome_no = 0
-    outcome_1_count = 0
+    mean = None  # Start mean off as null
+    outcome_no = 0  # start outcome number as 0
+    outcome_1_count = 0  # start count as 0
 
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no += 1  # Add 1 for every iteration
         if r == 1:
-            outcome_1_count += 1
-        mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
+            outcome_1_count += 1  # Add 1 if r == 1
+        mean = outcome_1_count / outcome_no  # calculate mean
+        chart.add_rows([mean])  # add mean to the chart
         time.sleep(0.05)
 
-    return mean
+    return mean  # return the mean value
 
 
+# Slider Widget
 number_of_trials = st.slider("Number of trials?", 1, 1000, 10)
 start_button = st.button("Run")
 
-if start_button:
+
+if start_button:  # Run if the start button is True (clicked)
     st.write(f"Running the experient of {number_of_trials} trials.")
     st.session_state["experiment_no"] += 1
     mean = toss_coin(number_of_trials)
